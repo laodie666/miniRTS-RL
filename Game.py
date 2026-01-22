7af725ac746d6f28f95ab4b472cefdfc8b53a6ba
shaoshe1 <jordan.shao@mail.utoronto.ca>
2026-01-21 23:18:23 -0500

    Slight work on step given action map

--- Game.py
+++ Game.py
@@ -18,7 +18,7 @@ WINDOW_H = MAP_H * BLOCKSIZE
 WINDOW_W = MAP_W * BLOCKSIZE
 
 
-# Tile info (player number, actor type, health_points, is_carrying_gold)
+# NOTE: Tile info (player number, actor type, health_points, is_carrying_gold)
     # Player 2 empty, 0 and 1 are players 3 bits
     
 NO_PLAYER = 2    
@@ -28,7 +28,12 @@ TC_HP = 10
 VILLAGER_HP = 2
 TROOP_HP = 3
 GOLD_HP = -1
+BARRACK_HP = 5
 
+VILLAGER_COST = 1
+TROOP_COST = 2
+TC_COST = 5
+BARRACK_COST = 3
     
 MAX_PLAYERS = 4
 MAX_ACTORS = 8
@@ -36,11 +41,12 @@ MAX_HP = 15
 CARRY_CAPACITY = 10
 
 # Actor type 4 bits
-EMPTY_TYPE = 4
+EMPTY_TYPE = 5
 TC_TYPE = 0
-BARRACK_TYPE = 1
-TROOP_TYPE = 2
-GOLD_TYPE = 3
+VILLAGER_TYPE = 1
+BARRACK_TYPE = 2
+TROOP_TYPE = 3
+GOLD_TYPE = 4
 
 # carry gold 1 bit
 
@@ -48,6 +54,7 @@ GOLD_TYPE = 3
 ASCII_CHARS = {
     TC_TYPE: 'T',       
     BARRACK_TYPE: 'B',  
+    VILLAGER_TYPE: 'V',
     TROOP_TYPE: 'S',    
     GOLD_TYPE: 'G',     
     EMPTY_TYPE: '.',    
@@ -60,6 +67,14 @@ PLAYER_COLORS = {
     NO_PLAYER: (200, 200, 200) # grey
 }
 
+# NOTE: The model will return an action map of action to take for each actor on every tile.
+ACT_STAY  = 0  
+ACT_UP    = 1
+ACT_DOWN  = 2
+ACT_LEFT  = 3
+ACT_RIGHT = 4
+TURN_TC = 5 # Turn villager to TC
+TURN_BARRACK = 6
 
 class tile:
 
@@ -120,6 +135,11 @@ def bitunpackTile(value: int):
 
     return tile(player_n, actor_type, hp, carry_gold)
 
+def newBarrackTile(side):
+    return tile(side, BARRACK_TYPE, BARRACK_HP, 2)
+
+def newTCTile(side):
+    return tile(side, TC_TYPE, TC_COST, 1)
 
 class RTSGame():
 
@@ -138,7 +158,8 @@ class RTSGame():
     
     # TODO
     # Training
-    #   Learn and set up DQN
+    #   Learn and set up basic policy gradient
+    #   Set up PPO
 
     def setScreen(self, screen):
         self.screen = screen
@@ -148,8 +169,8 @@ class RTSGame():
         
         self.map = np.full((MAP_W, MAP_H), empty_val)
 
-        self.map[2, 4] = bitpackTile(tile(0, TC_TYPE, TC_HP, 0))
-        self.map[7, 4] = bitpackTile(tile(1, TC_TYPE, TC_HP, 0))
+        self.map[2, 4] = bitpackTile(tile(0, TC_TYPE, TC_HP, 3))
+        self.map[7, 4] = bitpackTile(tile(1, TC_TYPE, TC_HP, 3))
 
         self.map[0, 4] = bitpackTile(tile(NO_PLAYER, GOLD_TYPE, GOLD_HP, 0))
         self.map[9, 4] = bitpackTile(tile(NO_PLAYER, GOLD_TYPE, GOLD_HP, 0))
@@ -161,7 +182,6 @@ class RTSGame():
 
 
     def drawGrid(self):
-
         for x in range(MAP_W):
             for y in range(MAP_H):
                 
@@ -201,6 +221,65 @@ class RTSGame():
                     sys.exit()
 
             pygame.display.update()
+        
+    def move_unit(map, cur_pos, target_pos):
+        map[target_pos] = map[cur_pos]
+        map[cur_pos] = bitpackTile(tile(NO_PLAYER, EMPTY_TYPE, 0, 0))
+        return map
+
+    def step(self, action, side):
+        empty_val = bitpackTile(tile(NO_PLAYER, EMPTY_TYPE, 0, 0))
+
+        for x in range(MAP_W):
+            for y in range(MAP_H):
+                tile_info = bitunpackTile(self.map[x][y])
+                if tile_info.player_n == side:
+                    tx, ty = x, y
+                    
+                    if action[x][y] == ACT_DOWN:
+                        ty += 1
+                    elif action[x][y] == ACT_UP:
+                        ty -= 1
+                    elif action[x][y] == ACT_LEFT:
+                        tx -= 1
+                    elif action[x][y] == ACT_RIGHT:
+                        tx += 1
+                    else:
+                        continue
+                    
+                    if not (0 <= tx < MAP_W and 0 <= ty < MAP_H):
+                        continue
+
+                    target_tile_info = bitunpackTile(self.map[tx][ty])
+                    # Now actually make a move.
+                    # TC makes villager
+                    if tile_info.actor_type == TC_TYPE:
+                        if target_tile_info.player_n == NO_PLAYER and tile_info.carry_gold >= VILLAGER_COST:
+                            self.map[tx, ty] = bitpackTile(tile(side, VILLAGER_TYPE, VILLAGER_HP, 0))
+                            tile_info.carry_gold -= VILLAGER_COST
+                            self.map[x, y] = bitpackTile(tile_info)
+
+                    # Barrack make troop
+                    
+                    # Villager collect, return gold, make tc and barrack
+                    elif tile_info.actor_type == VILLAGER_TYPE:
+                        if action[x][y] == TURN_BARRACK and tile_info.carry_gold >= BARRACK_COST:
+                            map[x][y] = bitpackTile(newBarrackTile())
+                        elif action[x][y] == TURN_TC and tile_info.carry_gold >= TC_COST:
+                            map[x][y] = bitpackTile(newTCTile())
+                        elif target_tile_info.actor_type == GOLD_TYPE:
+                            tile_info.carry_gold += 1
+                            self.map[x, y] = bitpackTile(tile_info)
+                        elif target_tile_info.player_n == NO_PLAYER:
+                            self.map = self.move_unit(self.map, (x, y), (tx, ty))
+                        elif target_tile_info.player_n == side and target_tile_info.actor_type == TC_TYPE or target_tile_info.actor_type == BARRACK_TYPE:
+                            target_tile_info.carry_gold += tile_info.carry_gold
+                            tile_info.carry_gold = 0
+                            self.map[x, y] = bitpackTile(tile_info)
+                            self.map[tx, ty] = bitpackTile(target_tile_info)
+                        
+                    # troop move and attack
+                    
 
 
 new = RTSGame()
